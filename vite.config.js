b49c2d2b9c0e0e8395e21db276bb6004f577e2b4
jaranacional11982 <jaranacional1@gmail.com>
2026-04-26 17:02:04 -0500

    corregir rutas para github-pages y local

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/https://jaranacional11982.github.io/mi-casita-react"
+  base: "/mi-casita-react/"
 });
